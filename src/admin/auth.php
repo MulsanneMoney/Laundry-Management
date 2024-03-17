23025188e4f27412c78eb145b2d44d30281b9d2f
MulsanneMoney <132583073+MulsanneMoney@users.noreply.github.com>
2024-03-17 22:11:46 +0200

    auth.php

--- src/admin/auth.php
+++ src/admin/auth.php
@@ -5,7 +5,7 @@ $link = mysqli_connect('localhost', 'root', 'mysql','lms');
 if (!$link) {
     die('Could not connect: ' . mysqli_connect_error());
 }
-	// include('db.php');
+	include('db.php');
 	$username=mysqli_real_escape_string($link, $_POST['username']);
 	$password=(mysqli_real_escape_string($link, $_POST['password']));
 	$query=mysqli_query($link,"SELECT * FROM admin1 WHERE username='$username' AND password='$password'");
